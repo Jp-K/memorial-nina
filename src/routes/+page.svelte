cefde989a84ce4ac24ef7d48666b562b0deb10b0
Jp-K <joaopaulokraisch@gmail.com>
2023-03-10 13:40:23 -0300

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,73 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+
+</script>
+
+<div>
+    <div class="header">
+        <ul>
+            <li>
+                <div>
+                    lorem
+                </div>
+            </li>
+            <li>
+                <a class="navbar-link">Início</a>
+            </li>
+            <li>
+                <a class="navbar-link">Bio</a>
+            </li>
+            <li>
+                <a class="navbar-link">Fotos</a>
+            </li>
+            <li>
+                <a class="navbar-link">Chat</a>
+            </li>
+        </ul>
+    </div>
+    <div class="content">
+        
+    </div>
+    <div class="chat">
+
+    </div>
+</div>
+
+<!-- #4F378B primary -->
+<!-- #4A4458 secondary -->
+<!-- #633B48 tertiary -->
+<!-- #8C1D18 error -->
+<!-- #1C1B1F surface -->
+<!-- #49454F surface-Variant -->
+<style>
+
+    .header {
+        position: absolute;
+        top: 0;
+        width: 100%;
+    }
+
+    .header ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        background-color: #1C1B1F;
+    }
+
+    .header ul li {
+        float: left;
+    }
+
+    .navbar-link {
+        background-color: #1C1B1F;
+        color: white;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+        display: block;
+    }
+
+    .navbar-link:hover {
+        background-color: #4F378B;
+    }
+</style>
